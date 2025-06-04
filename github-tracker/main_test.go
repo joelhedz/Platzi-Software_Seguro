8b867cdb2008a8c7ff670436fd7797d828fd369c
joelhedz <joelhumberto334@gmail.com>
2025-06-04 16:33:12 -0600

    Add Mock Test

--- github-tracker/main_test.go
+++ github-tracker/main_test.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"github-tracker/github-tracker/models"
+	reporsitory "github-tracker/github-tracker/repository"
+	"github-tracker/github-tracker/repository/entity"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,4 +19,53 @@ func TestDummy(t *testing.T) {
 	result := 22
 
 	c.Equal(22, result)
+}
+
+func TestInsert(t *testing.T) {
+	c := require.New(t)
+
+	webhook := models.GitHubWebhook{
+		Repository: models.Repository{
+			FullName: "joelhedz/Platzi-Software_Seguro",
+		},
+		HeadCommit: models.Commit{
+			ID:      "5537ee78e64bce92928ce932fd123683f04a85d3",
+			Message: "Fix Checkout version on github action",
+			Author: models.CommitUser{
+				Email:    "joelhumberto334@gmail.com",
+				Username: "joelhedz",
+			},
+		},
+	}
+
+	body, err := json.Marshal(webhook)
+	if err != nil {
+		c.NoError(err)
+	}
+
+	cretedTime := time.Now()
+
+	m := mock.Mock{}
+
+	mockCommit := reporsitory.MockCommit{Mock: &m}
+
+	commit := entity.Commit{
+		RepoName:       webhook.Repository.FullName,
+		CommitId:       webhook.HeadCommit.ID,
+		CommitMessage:  webhook.HeadCommit.Message,
+		AuthorUsername: webhook.HeadCommit.Author.Username,
+		AuthorEmail:    webhook.HeadCommit.Author.Email,
+		Payload:        string(body),
+		CreatedAt:      cretedTime,
+		UpdatedAt:      cretedTime,
+	}
+
+	ctx := context.Background()
+
+	mockCommit.On("Insert", ctx, &commit).Return(nil)
+
+	err = insertGitHubWebhook(ctx, mockCommit, webhook, string(body), cretedTime)
+	c.NoError(err)
+
+	m.AssertExpectations(t)
 }
